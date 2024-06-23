a61892110b28f1593d2e1d108fc1e537083a223a
Multi538 <59820274+Multi538@users.noreply.github.com>
2024-06-23 13:06:12 +0200

    score system now updates every second

--- Projekt_Computergrafik/GameController.gd
+++ Projekt_Computergrafik/GameController.gd
@@ -1,11 +1,27 @@
-extends Node2D
+extends Node
 
+# Declare variables
+var score: int = 0
+var score_timer: Timer
 
-# Called when the node enters the scene tree for the first time.
+# Reference to the score label
+@onready var score_label: Label = $ScoreLabel
+
+# Called when the node enters the scene tree
 func _ready():
-	pass # Replace with function body.
+	# Create and configure the timer
+	score_timer = Timer.new()
+	score_timer.wait_time = 1.0
+	score_timer.one_shot = false
+	score_timer.connect("timeout", Callable(self, "_on_score_timer_timeout"))
+	add_child(score_timer)
+	score_timer.start()
 
+# Called every second by the timer
+func _on_score_timer_timeout():
+	score += 1
+	update_score_label()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+# Function to update the score label
+func update_score_label():
+	score_label.text = "score: " + str(score)
