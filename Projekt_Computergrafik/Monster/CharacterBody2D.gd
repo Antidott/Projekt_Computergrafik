0d05bb0103414ad24a4884f39d4231900136f6d9
MoglyGit <mike-feist@t-online.de>
2024-07-11 23:48:19 +0200

    monster goes away from fire, when stand in fire (no only stopping)

--- Projekt_Computergrafik/Monster/CharacterBody2D.gd
+++ Projekt_Computergrafik/Monster/CharacterBody2D.gd
@@ -11,12 +11,18 @@ func _physics_process(delta):
 	var rng = RandomNumberGenerator.new()
 	for fire in targetFire.get_children():
 		if targetPlayer.global_position.distance_to(fire.global_position) < 100:#stop hunting if palyer is in fire range
+			if global_position.distance_to(fire.global_position) < 300:
+				var direction = -(fire.global_position - global_position).normalized()
+				goAwayPoint = direction
+				look_at(goAwayPoint)
+				velocity = direction * speed
+				move_and_slide()
 			return
 		var distanceToFire = global_position.distance_to(fire.global_position)
 		if ignorePlayerHunt and distanceToFire < 190:#go away if to close on fire
 			if(goAwayPoint == null):
 				var direction = -(fire.global_position - global_position).normalized()
-				goAwayPoint = direction;
+				goAwayPoint = direction
 				velocity = direction * speed
 			look_at(goAwayPoint)
 			move_and_slide()
